2d07c829c6a70cf19e559b9a0278d49b2a941cd0
marianasaikali <mariana_saikali@hotmail.com>
2021-01-15 21:41:47 +0000

    done

--- mariana/pgzero1.py
+++ mariana/pgzero1.py
@@ -1,4 +1,5 @@
-from pgzrun import *
+import pgzrun
+
 
 alien = Actor('alien')
 alien.pos = 100, 56
@@ -9,3 +10,16 @@ HEIGHT = alien.height + 20
 def draw():
     screen.clear()
     alien.draw()
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
+
+pgzrun.go()
